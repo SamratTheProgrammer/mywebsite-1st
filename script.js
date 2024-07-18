91427f63578a1d189f70164d4ae6cf19f0742305
SamratTheProgrammer <166220572+SamratTheProgrammer@users.noreply.github.com>
2024-07-18 20:36:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+ScrollReveal({
+    //reset: true,
+    distance:'80px',
+    duration: 2000,
+    delay: 200
+
+});
+
+ScrollReveal().reveal('.wishes-content h3, .heading2, .home-content', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .wishes-content, .memories-content', {origin: 'bottom'});
+ScrollReveal().reveal('.heading, .memories-img, .wishes-content', {origin: 'left'});
+
+//typed
+
+
+const typed= new Typed('.multiple-text',{
+    strings:['Barsha','Darling'],
+    typeSpeed:100,
+    backSpeed:200,
+    backDelay:200,
+    loop:true
+})
